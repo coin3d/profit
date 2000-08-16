f04a2a7eb06a778f6f767d28f4b35b8ff27d6b6b
Lars J. Aas <larsa@sim.no>
2000-08-16 10:06:20 +0000

    Upgrade to autoconf 2.49a.  Synchronization with changes under conf-macros/.

--- autogen.sh
+++ autogen.sh
@@ -3,42 +3,65 @@
 # Author: Morten Eriksen, <mortene@sim.no>. Loosely based on Ralph
 # Levien's script for Gnome.
 
-DIE=0
+DIE=false
 
 PROJECT=Profit
 
-# FIXME: check for minimum version number? 19990822 mortene.
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
-        echo
-        echo "You must have autoconf installed to generate"
-	echo "configure information and Makefiles for $PROJECT."
-        echo "Get ftp://ftp.gnu.org/pub/gnu/autoconf-*.tar.gz"
-        DIE=1
-}
+AUTOCONF_VER=2.49a
+AUTOMAKE_VER=1.4a
+LIBTOOL_VER=1.3.5
 
 # FIXME: check for minimum version number? 19990822 mortene.
-(libtool --version) < /dev/null > /dev/null 2>&1 || {
-        echo
-        echo "You must have libtool installed to generate"
-	echo "configure information and Makefiles for $PROJECT."
-        echo "Get ftp://ftp.gnu.org/pub/gnu/libtool-*.tar.gz"
-        DIE=1
-}
+if test -z "`autoconf --version | grep \" $AUTOCONF_VER\" 2> /dev/null`"; then
+  cat <<EOF
 
-# FIXME: check for minimum version number? 19990822 mortene.
-(automake --version) < /dev/null > /dev/null 2>&1 || {
-        echo
-        echo "You must have automake installed to generate"
-	echo "configure information and Makefiles for $PROJECT."
-        echo "Get ftp://ftp.gnu.org/pub/gnu/automake-*.tar.gz"
-        DIE=1
-}
+  Invalid Version of Autoconf
+  ---------------------------
+  You must use the CVS development version of autoconf ($AUTOCONF_VER)
+  to generate configure information and Makefiles for $PROJECT.
+  You can find the pre-release snapshot at:
+
+  ftp://alpha.gnu.org/gnu/autoconf/autoconf-2.49a.tar.gz
+
+EOF
+  DIE=true
+fi
+
+if test -z "`automake --version | grep \" $AUTOMAKE_VER\" 2> /dev/null`"; then
+  cat <<EOF
+
+  Invalid Version of Automake
+  ---------------------------
+  You must use the CVS development version of automake to ($AUTOMAKE_VER)
+  to generate configure information and Makefiles for $PROJECT.
+
+  The CVS automake repository can be fetched by running the following
+  set of commands:
+
+  $ cvs -d :pserver:anoncvs@anoncvs.cygnus.com:/cvs/automake login
+  $ cvs -d :pserver:anoncvs@anoncvs.cygnus.com:/cvs/automake co automake
+
+EOF
+  DIE=true
+fi
+
+if test -z "`libtool --version | grep \" $LIBTOOL_VER \" 2> /dev/null`"; then
+  cat <<EOF
+
+  Invalid Version of Libtool
+  --------------------------
+  You must have libtool version $LIBTOOL_VER installed to generate
+  configure information and Makefiles for $PROJECT.
+
+  Get ftp://ftp.gnu.org/pub/gnu/libtool/libtool-1.3.5.tar.gz
+
+EOF
+  DIE=true
+fi
 
 # FIXME: check for more tools? 19990822 mortene.
 
-if test "$DIE" -eq 1; then
-        exit 1
-fi
+$DIE && exit 1
 
 echo "Running aclocal..."
 aclocal
