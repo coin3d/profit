ec48c48cd93510100f8609d30f424bc3887a286b
Lars J. Aas <larsa@sim.no>
2001-05-04 16:51:36 +0000

    New file for keeping binary compatibility with obsolete interfaces.

---
+++ src/cruft.c
@@ -0,0 +1,44 @@
+/**************************************************************************\
+ * 
+ *  Copyright (C) 2001 by Systems in Motion.  All rights reserved.
+ *
+ *  This file is part of the profit library.
+ *
+ *  This library (profit) is free software and comes without any warranty.
+ *  You can redistribute it and / or modify it under the terms of the
+ *  GNU General Public License, version 2, as published by the
+ *  Free Software Foundation (see file named COPYING for details).
+ *
+ *  If you want to use profit in applications not covered by licenses
+ *  compatible with the GNU GPL, you can contact SIM to acquire a
+ *  Professional Edition license for profit.
+ *
+ *  Systems in Motion AS, Prof. Brochs gate 6, N-7030 Trondheim, NORWAY
+ *  http://www.sim.no/ sales@sim.no Voice: +47 22114160 Fax: +47 67172912
+ *
+\**************************************************************************/
+
+/* $Id$ */
+
+#include <config.h>
+
+/*
+ * This file contains old interface functions.  These functions have been
+ * made obsolete by other functions in the interface, but they are still
+ * compiled into the library for binary compatibility reasons.  These
+ * functions are no longer declared in the profit header files.
+ *
+ * For an explanation of the current/revision/age interface versioning
+ * system, look up "Versioning" in the Libtool manual.  That is the system
+ * being used, except for the fact that we include "old cruft" to be able
+ * to keep the age count non-zero even when we remove functions from the
+ * interface.
+ */
+
+/***************************************************************************
+ * Cruft from Interface 0:
+ *
+ * - The current interface version *is* 0, so there is no cruft yet.
+ */
+
+/**************************************************************************/
