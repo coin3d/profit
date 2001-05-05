709eb67679d303e86b21e5e2757980d28e915723
Lars J. Aas <larsa@sim.no>
2001-05-05 16:54:13 +0000

    Header merge.

--- include/profit/basics.h
+++ include/profit/basics.h
@@ -20,14 +20,75 @@
 
 /* $Id$ */
 
-#ifndef PRF_BASIC_FUNCS_H
-#define PRF_BASIC_FUNCS_H
+#ifndef PRF_BASICS_H
+#define PRF_BASICS_H
 
-#include <profit/basic_types.h>
+#include <sys/types.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
+
+#ifdef PROFIT_MAKE_DLL
+#define PROFIT_API __declspec(dllexport)
+#else /* ! PROFIT_MAKE_DLL */
+#ifdef PROFIT_DLL
+#define PROFIT_API __declspec(dllimport)
+#else /* ! PROFIT_DLL */
+#define PROFIT_API
+#endif /* ! PROFIT_DLL */
+#endif /* ! PPROFIT_MAKE_DLL */
+
+#if defined(__sgi) || defined(__hpux)
+#define HAVE_INTTYPES
+#define HAVE_UINTTYPES
+#endif
+
+#ifdef __BIT_TYPES_DEFINED__
+#define HAVE_INTTYPES
+#endif /* __BIT_TYPES_DEFINES__ */
+
+#ifndef HAVE_INTTYPES
+#define HAVE_INTTYPES
+typedef  signed char            int8_t;
+typedef  signed short           int16_t;
+typedef  signed int             int32_t;
+#endif /* ! HAVE_INTTYPES */
+
+#ifndef HAVE_UINTTYPES
+#define HAVE_UINTTYPES
+typedef  unsigned char          uint8_t;
+typedef  unsigned short         uint16_t;
+typedef  unsigned int           uint32_t;
+#endif /* ! HAVE_UINTTYPES */
+
+typedef  float                  float32_t;
+typedef  double                 float64_t;
+
+typedef  int                    bool_t;
+
+typedef  struct bfile_s         bfile_t;
+
+typedef  struct prf_model_s     prf_model_t;
+typedef  struct prf_state_s     prf_state_t;
+
+typedef  struct prf_node_s      prf_node_t;
+typedef  struct prf_nodeinfo_s  prf_nodeinfo_t;
+
+typedef  struct prf_vertex_s    prf_vertex_t;
+typedef  struct prf_material_s  prf_material_t;
+typedef  struct prf_texture_s   prf_texture_t;
+
+typedef  float32_t              matrix4x4_f32_t[4][4];
+
+#ifndef FALSE
+#define FALSE                   (0)
+#define TRUE                    (!FALSE)
+#endif /* ! FALSE */
+
+#ifndef NULL
+#define NULL                    (0)
+#endif /* ! NULL */
 
 #define PRF_MAX( arg1, arg2 ) (((arg1) < (arg2)) ? (arg2) : (arg1))
 #define PRF_MIN( arg1, arg2 ) (((arg1) < (arg2)) ? (arg1) : (arg2))
@@ -47,8 +108,8 @@ extern "C" {
     } while ( FALSE )
 #define prf_dblread( fA ) _prf_read_double( &(fA) )
 #define prf_dblwrite( fD, fA ) _prf_write_double( &(fD), fA )
-float64_t _prf_read_double( float64_t * fA );
-void _prf_write_double( float64_t * fD, float64_t fA );
+PROFIT_API  float64_t _prf_read_double( float64_t * fA );
+PROFIT_API  void _prf_write_double( float64_t * fD, float64_t fA );
 #else /* !__sgi && !__hpux*/
 #define prf_dblcpy( fD, fA ) (fD) = (fA)
 #define prf_dblread( fA ) (fA)
@@ -59,4 +120,4 @@ void _prf_write_double( float64_t * fD, float64_t fA );
 }; /* extern "C" */
 #endif /* __cplusplus */
 
-#endif /* ! PRF_BASIC_FUNCS_H */
+#endif /* ! PRF_BASICS_H */
