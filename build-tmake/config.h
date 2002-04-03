ba7e8fab72f912180e984aa1c15a7f53bb9c8f6c
Marius Kintel <kintel@sim.no>
2002-04-03 14:36:03 +0000

    Regenerated

--- build-tmake/config.h
+++ build-tmake/config.h
@@ -1,12 +1,33 @@
-/* include/profit/config.h.in.  Generated automatically from configure.in by autoheader.  */
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.ac by autoheader.  */
 
-/* Define if your processor stores words with the most significant
-   byte first (like Motorola and SPARC, unlike Intel and VAX).  */
-#undef WORDS_BIGENDIAN
+/* Define if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
 
 /* Name of package */
-#define PACKAGE libprofit
+#define PACKAGE "profit"
+
+/* the current profit interface age */
+#define PROFIT_INTERFACE_AGE 0
+
+/* the current profit interface revision */
+#define PROFIT_INTERFACE_REVISION 0
+
+/* the current profit interface version */
+#define PROFIT_INTERFACE_VERSION 0
+
+/* the major part of the profit release version number */
+#define PROFIT_MAJOR_VERSION 0
+
+/* the micro part of the profit release version number */
+#define PROFIT_MICRO_VERSION 1
+
+/* the minor part of the profit release version number */
+#define PROFIT_MINOR_VERSION 9
 
 /* Version number of package */
-#define VERSION 0.9.0
+#define VERSION "0.9.1"
 
+/* Define if your processor stores words with the most significant byte first
+   (like Motorola and SPARC, unlike Intel and VAX). */
+/* #undef WORDS_BIGENDIAN */
